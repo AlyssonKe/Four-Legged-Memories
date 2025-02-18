9394099e7a2ae31f478432e07f0795760e54dbf1
AlyssonKe <alyssonkepm87@gmail.com>
2025-02-17 21:38:38 -0300

    Change dialogues to english

--- src/ReplicatedStorage/Client/Controllers/Guis/Dialogue/DialoguesData/Part1.lua
+++ src/ReplicatedStorage/Client/Controllers/Guis/Dialogue/DialoguesData/Part1.lua
@@ -4,22 +4,22 @@ return {
 		["P1-1"] = {
 			character = "???",
 			duration = 4.3,
-			subtitle = "[???]: Foi você que me salvou amigão!",
+			subtitle = "[???]: It was you who saved me, friend!",
 		},
 		["P1-2"] = {
 			character = "Bruce",
 			duration = 5,
-			subtitle = "[Bruce]: Hmm, que lugar é esse?",
+			subtitle = "[Bruce]: Hmm, what is this place?",
 		},
 		["P1-3"] = {
 			character = "Bruce",
 			duration = 5,
-			subtitle = "[Bruce]: Não parece a casa do papai.",
+			subtitle = "[Bruce]: It doesn't look like daddy's house.",
 		},
 		["P1-4"] = {
 			character = "Bruce",
 			duration = 5,
-			subtitle = "[Bruce]: Quem está aí?",
+			subtitle = "[Bruce]: Who is there?",
 		},
 	},
 }
